88ec2035527636317b6c2331fde9cf9e47b49156
Tyler Nguyen <nguyenthanhtai20052000@gmail.com>
2024-08-29 15:56:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Gửi Bý Me",
+    introTitle: 'ebe My iu dấu của anh',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu tui 🥰',
+    desc: 'Thoát web là chấp nhận yêu tui rồi nhé!!!',
+    btnYes: 'đúng dị <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Anh iu bé nhiều nhiều 😘😘',
+    messDesc: 'Tối nay, anh qua đón bé đi chơi nha hêhheeee không đi được thì mình call ha.',
+    btnAccept: 'Okiiiii lun <3',
+   
+}
